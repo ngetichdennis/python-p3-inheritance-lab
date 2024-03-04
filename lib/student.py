ffbcd7a2754df996504170d0bb2996bd1470b558
Dennis <ngetichdennis90@gmail.com>
2024-03-04 12:09:46 +0300

    completed assignment

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,7 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def learn(self,string):
+        super().__init__
+        self.knowledge = string  # Add knowledge to the student's knowledge base
+        
--- lib/teacher.py
+++ lib/teacher.py
@@ -3,8 +3,22 @@
 from user import User
 
 import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]
+        else:
+            self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
